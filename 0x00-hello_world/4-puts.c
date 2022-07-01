82ce276f6a6af370555ea51cc09785e412551070
anthonyidyu <anthonysynod@gmail.com>
2022-07-01 14:45:07 -0700

    resolving trailing whitespace error and main function descript

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,29 +1,17 @@
 #include <stdio.h>
-
 #include <string.h>
-
- 
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
- 
-
+ * main - Entry point
+ * Description: The programmer's life quote
+ * Return: Always 0 (success)
+*/
 int main(void)
-
 {
+	char str1[52];
 
-	puts("\"Programming is like building a multilingual puzzle");
+	strcpy(str1, "\"Programming is like building a multilingual puzzle");
 
-	 
+	puts(str1);
 
 	return (0);
-
 }

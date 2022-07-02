ba907b42675c9c57c1082e963d856e84183c9cd3
anthonyidyu <anthonysynod@gmail.com>
2022-07-02 04:45:04 -0700

    resolving trailing whitespace error iii

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,33 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+/* more headers goes there */
 
-/**
- * main - Prints a text according number
- *
- *
- * Return: Always (Success)
- *
- */
+/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
-	int lastn;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastd = n % 10;
-
-	if (lastd > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
-	}
-	else if (lastd == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, lastd);
-	} else
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastn);
-	}
+	/* your code goes there */
 	return (0);
 }

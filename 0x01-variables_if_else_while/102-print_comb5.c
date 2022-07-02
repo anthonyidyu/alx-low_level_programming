173e36c97134dc0535d148a0de67633a3e765193
anthonyidyu <anthonysynod@gmail.com>
2022-07-02 06:55:25 -0700

    tracing whitespace errors resolved

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,29 +1,30 @@
 #include <stdio.h> 
 
 /**
- * main - prints all possible combinations of two two-digit numbers
- * Return: Always 0 (Success)
+ * main - entry point
+ * 
+ * Return: alway returns 0
+ *
  */
 int main(void)
 {
-	int i, j;
+	int i;
+	int j;
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (j = 0; j < 100; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			if (i < j)
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+
+			if (!(i == 98 && j == 99))
 			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
+				putchar(',');
 				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
 			}
 		}
 	}

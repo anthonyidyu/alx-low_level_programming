980cbc7a7c52ff5f1311b44d9061038c0febf0e1
anthonyidyu <anthonysynod@gmail.com>
2022-07-05 04:59:19 -0700

    resolving trailing whitespace error iii

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,26 +1,39 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <unistd.h>
 /**
- * main - entry point 
- * Return: alway returns 0
+ * main - Entry point
+ * prints all possible combination of 2-digit numbers
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	int i;
-	int j;
-	for (i = 0; i <= 99; i++)
+	int c, i, k, j;
+	for (c = 48; c <= 57; c++)
 	{
-		for (j = i + 1; j <= 99; j++)
+		for (i = 48; i <= 57; i++)
 		{
-			putchar(i / 10 + '0');
-			putchar(i % 10 + '0');
-			putchar(' ');
-			putchar(j / 10 + '0');
-			putchar(j % 10 + '0');
-
-			if (!(i == 98 && j == 99))
+			for (k = 48; k <= 57; k++)
 			{
-				putchar(',');
-				putchar(' ');
+				for (j = 48; j <= 57; j++)
+				{
+					if (((k + j) > (c + i) &&  k >= c) || c < k)
+					{
+						putchar(c);
+						putchar(i);
+						putchar(' ');
+						putchar(k);
+						putchar(j);
+						if (c + i + k + j == 227 && c == 57)
+						{
+							break;
+						}
+						else
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
 			}
 		}
 	}

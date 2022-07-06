c37784b092d59c7b6786d821842480981e9e02e2
anthonyidyu <anthonysynod@gmail.com>
2022-07-06 09:39:52 -0700

    Pj 0x02 Done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints a to z and a new line when called
+ *
+ * Return: 0 Always (success)
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+		{
+			_putchar('\n');
+		}
+	}
+}

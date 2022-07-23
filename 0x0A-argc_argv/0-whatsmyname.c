92c56704548b1e1b786daff7a3b8c89d43777a64
Anthony Idyu <anthonysynod@gmail.com>
2022-07-23 15:41:50 +0100

    getting a hang on passing arguments to my program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,12 +4,12 @@
  * main - prints program name, followed by a new line
  * @argc: int
  * @argv: list
- * Returna: 0
+ * Return: 0
  */
 
 int main(int argc, char const *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
-	return (0);
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
 }

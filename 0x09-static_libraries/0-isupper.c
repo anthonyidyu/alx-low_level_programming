202df19fc8c859172f1399a06a58f7fb93844cec
Anthony Idyu <anthonysynod@gmail.com>
2022-07-26 20:21:25 +0100

    what how and why are static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks if input is an uppercase letter
+ * @c: user input
+ * Return: 1 if input is uppercase and 0 if otherwise
+ */
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

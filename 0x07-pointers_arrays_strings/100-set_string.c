357ab56228423179c02c6f0a4a2842ea885eb6c6
anthonyidyu <anthonysynod@gmail.com>
2022-07-30 06:06:28 -0700

    update 0x07-pas

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,8 +2,8 @@
 
 /**
  * set_string - sets the value of a pointer to a char
- * @s: pointer to pointer
- * @to: pointer char
+ * @s: the pointer
+ * @to: new value
  */
 
 void set_string(char **s, char *to)

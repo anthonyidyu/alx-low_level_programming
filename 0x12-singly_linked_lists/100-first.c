69f9c6188032f7f405ac0e528c4a8f7c119a5d43
Anthony Idyu <anthonysynod@gmail.com>
2022-08-12 06:35:30 +0100

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@ void __attribute__((constructor)) hare(void);
 
 /**
  * hare - Prints a string before the
- *        main function is executed.
+ * main function is executed.
  */
 void hare(void)
 {

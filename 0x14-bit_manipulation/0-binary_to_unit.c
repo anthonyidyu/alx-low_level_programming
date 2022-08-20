e64bab28559bad04597453a2274340dad337e9f3
Anthony Idyu <anthonysynod@gmail.com>
2022-08-20 03:55:19 +0100

    0x14

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -1,37 +1,20 @@
 #include "main.h"
-
 /**
- * binary_to_uint - converts a binary number to an
- * unsigned int.
- * @b: binary.
- *
- * Return: unsigned int.
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
-
-	if (!b)
-		return (0);
-
-	ui = 0;
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
-	{
-		if (b[len] != '0' && b[len] != '1')
-		{
-			return (0);
-		}
-
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
-	}
-
-	return (ui);
+int i;
+unsigned int digit = 0;
+if (!b)
+return (0);
+for (i = 0; b[i]; i++)
+{
+if (b[i] != '0' && b[i] != '1')
+return (0);
+digit = 2 * digit + (b[i] - '0');
+}
+return (digit);
 }
